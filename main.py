b3703fc67193b569cd49cba0de90aa9b43413498
Thiago Sposito <sposito.thiago@gmail.com>
2022-06-11 01:12:08 -0300

    highpass variable refactor in name

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ def main():
         fft = np.fft.fftshift(fft)
         (h, w) = size
         (cX, cY) = (int(w / 2), int(h / 2))
-        low_pass_win_size = 512
-        fft[cY - low_pass_win_size:cY + low_pass_win_size, cX - low_pass_win_size:cX + low_pass_win_size] = 0
+        high_pass_win_size = 512
+        fft[cY - high_pass_win_size:cY + high_pass_win_size, cX - high_pass_win_size:cX + high_pass_win_size] = 0
         fft_shift = np.fft.ifftshift(fft)
         recon = np.fft.ifft2(fft_shift)
         magnitude = 20 * np.log(np.abs(recon))
