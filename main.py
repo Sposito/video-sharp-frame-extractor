c2c5a43c8dde0fafe747385f66592f0e9bd0f07a
Thiago Sposito <sposito.thiago@gmail.com>
2022-06-11 03:34:01 -0300

    use Laplacian to detect sharpness

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import argparse
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-i', '--input', type=str, required=True, help='path to input video')
-ap.add_argument('-s', '--steps', type=str, default=30, help='the number of frame from where it picks the sharpest,'
+ap.add_argument('-s', '--steps', type=int, default=30, help='the number of frame from where it picks the sharpest,'
                                                             ' defaults to 30')
 args = vars(ap.parse_args())
 
@@ -24,29 +24,22 @@ def main():
     step = args['steps']
     ini_time = time.time()
     success, img = vid.read()
+    csv = 'Filename,id,frame\n'
 
     while success:
         size = (int(img.shape[1]/2), int(img.shape[0]/2))
-        img = cv2.resize(img, size)
-        fft = np.fft.fft2(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
-        fft = np.fft.fftshift(fft)
-        (h, w) = size
-        (cX, cY) = (int(w / 2), int(h / 2))
-        high_pass_win_size = 512
-        fft[cY - high_pass_win_size:cY + high_pass_win_size, cX - high_pass_win_size:cX + high_pass_win_size] = 0
-        fft_shift = np.fft.ifftshift(fft)
-        recon = np.fft.ifft2(fft_shift)
-        magnitude = 20 * np.log(np.abs(recon))
-        mean = np.mean(magnitude)
+        # img = cv2.resize(img, size)
+        mean = cv2.Laplacian(img, cv2.CV_64F).var()
         win_counter = frame_counter // step
 
         if win_counter != current_window_frame:
-            cv2.imwrite(f'frames/{win_counter:04}.png', current_img)
+            file_name = f'frames/{win_counter:04}_{mean}.png'
+            cv2.imwrite(file_name, current_img)
             highest_mean = -1
             current_window_frame = win_counter
             time_estimate = f'{(time.time() - ini_time) / frame_counter * total_frames/60:.2f}m remaining'
             print(f'Progress: {highest_counter/total_frames*100.0:.2f}% | {time_estimate}')
-
+            csv = f'{csv}{file_name},{win_counter},{highest_counter}\n'
         if mean > highest_mean:
             highest_mean = mean
             current_img = img
@@ -54,6 +47,8 @@ def main():
 
         success, img = vid.read()
         frame_counter += 1
+    with open('log.csv', 'w') as file:
+        file.write(csv)
 
 
 if __name__ == '__main__':
